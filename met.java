5f5c6408ca875ace41666de963b94d3fae599706
Klaszlo5 <94529898+Klaszlo5@users.noreply.github.com>
2023-02-23 16:02:30 +0100

    Update met.java

--- met.java
+++ met.java
@@ -12,7 +12,7 @@ public class Metodusok {
         szamn=osszead(1,osszead(1, osszead(4, 4)));
         kiir(szamn);  
         kiir(Math.sqrt(osszead(1, osszead(3, 2)))));   
-        int a = 1, b = 2, c = 3;
+        int a = 1, b = 2, c = 3, d = 4;
         kiir(osszead(a, b);
         int osszeadszamok = osszead(osszead(1, 2), osszead(3, 4));
         kiir(a,b,c,d,osszeadszamok);
